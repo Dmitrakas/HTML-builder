b89459e4f7a2573ce0986f49654cfcd9fe8c66bb
Dmitrakas <zhorovind@gmail.com>
2022-11-06 19:23:32 +0300

    feat: add 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const { mkdir, copyFile, readdir, rm } = require('fs/promises');
+const path = require('path');
+const filesPath = path.join(__dirname, 'files');
+const filesCopyPath = path.join(__dirname, 'files-copy');
 
+
+async function createCopyDirectory() {
+  try {
+    await mkdir(filesCopyPath, { recursive: true });
+    await clearCopyDirectory();
+
+    const files = await readdir(filesPath);
+
+    for (let file of files) {
+      await copyFile(path.join(filesPath, file), path.join(filesCopyPath, file));
+    }
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function clearCopyDirectory() {
+  const filesCopyDirectory = await readdir(filesCopyPath);
+
+  for (let file of filesCopyDirectory) {
+    await rm(path.join(filesCopyPath, file), { force: true });
+  }
+}
+
+createCopyDirectory();
